7234db7a436fea5bee105f3fad3dc02ababc2b11
Shital Tukaram Ambekar <shitalambekar013@gmail.com>
2026-07-20 20:31:53 +0530

    Delete Day01/comments.py

--- Day01/comments.py
+++
@@ -1,8 +0,0 @@
-# ----------------------------------------
-# Day 01 - Comments in Python
-# ----------------------------------------
-
-# This is a single-line comment
-
-print("Python ignores comments.")
-print("Comments make code easier to understand.")
