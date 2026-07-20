1c7407fdcba2773454556cd7f10a0decbb26116d
Shital Tukaram Ambekar <shitalambekar013@gmail.com>
2026-07-20 23:14:36 +0530

    Create user_input.py
    Add Day02 User Input program

---
+++ Day02/user_input.py
@@ -0,0 +1,12 @@
+# ----------------------------------------
+# Day 02 - User Input
+# Author: Shital Tukaram Ambekar
+# Description: Taking input from the user.
+# ----------------------------------------
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+print("\n----- Student Information -----")
+print("Name :", name)
+print("Age  :", age)
