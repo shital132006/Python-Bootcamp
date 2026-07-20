23d9b771f193ce7153d79e57a1cc2b8045f93921
Shital Tukaram Ambekar <shitalambekar013@gmail.com>
2026-07-20 23:17:38 +0530

    Create type_casting.py
    Add Day02 Type Casting program

---
+++ Day02/type_casting.py
@@ -0,0 +1,12 @@
+# ----------------------------------------
+# Day 02 - Type Casting
+# Author: Shital Tukaram Ambekar
+# Description: Converting string input into integers.
+# ----------------------------------------
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+total = num1 + num2
+
+print("Sum =", total)
