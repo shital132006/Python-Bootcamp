239874f28f48b9dbd68eadafb20331b4334c285e
Shital Tukaram Ambekar <shitalambekar013@gmail.com>
2026-07-20 20:44:57 +0530

    Create data_types.py
    Add Day01 Data Types program

---
+++ Day01/data_types.py
@@ -0,0 +1,19 @@
+# ----------------------------------------
+# Day 01 - Data Types
+# Author: Shital Tukaram Ambekar
+# ----------------------------------------
+
+name = "Shital"
+age = 20
+cgpa = 8.78
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("CGPA:", cgpa)
+print("Student:", is_student)
+
+print(type(name))
+print(type(age))
+print(type(cgpa))
+print(type(is_student))
