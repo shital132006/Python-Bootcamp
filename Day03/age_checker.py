3636709a071ba9704cf8be881878875d4bdd888a
Shital Tukaram Ambekar <shitalambekar013@gmail.com>
2026-07-20 23:22:21 +0530

    Create age_checker.py
    Add Day03 Age Checker program

---
+++ Day03/age_checker.py
@@ -0,0 +1,12 @@
+# ----------------------------------------
+# Day 03 - Age Checker
+# Author: Shital Tukaram Ambekar
+# Description: Check voting eligibility.
+# ----------------------------------------
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
