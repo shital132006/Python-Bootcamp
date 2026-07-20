b33318529bab6227e3a3e50516efa9834e2b1a7b
Shital Tukaram Ambekar <shitalambekar013@gmail.com>
2026-07-20 23:19:53 +0530

    Create simple_calculator.py
    Add Day02 Simple Calculator program

---
+++ Day02/simple_calculator.py
@@ -0,0 +1,14 @@
+# ----------------------------------------
+# Day 02 - Simple Calculator
+# Author: Shital Tukaram Ambekar
+# Description: Basic calculator using user input.
+# ----------------------------------------
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("\n----- Calculator -----")
+print("Addition       :", num1 + num2)
+print("Subtraction    :", num1 - num2)
+print("Multiplication :", num1 * num2)
+print("Division       :", num1 / num2)
