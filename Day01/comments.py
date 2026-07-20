29906b17cf3a43ac41798492bace1a4cd5b11f53
Shital Tukaram Ambekar <shitalambekar013@gmail.com>
2026-07-20 20:25:49 +0530

    Create comments.py
    Add Day01 Comments program

---
+++ Day01/comments.py
@@ -0,0 +1,8 @@
+# ----------------------------------------
+# Day 01 - Comments in Python
+# ----------------------------------------
+
+# This is a single-line comment
+
+print("Python ignores comments.")
+print("Comments make code easier to understand.")
