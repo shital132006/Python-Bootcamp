b282870eacbf32284f1d1c9b6c2b67195aad58a3
Shital Tukaram Ambekar <shitalambekar013@gmail.com>
2026-07-20 20:23:56 +0530

    Create hello_world.py
    Add Day01 Hello World program

---
+++ Day01/hello_world.py
@@ -0,0 +1,8 @@
+# ----------------------------------------
+# Day 01 - Hello World Program
+# Author: Shital Tukaram Ambekar
+# ----------------------------------------
+
+print("Hello, World!")
+print("Welcome to Python Programming!")
+print("This is my first Python program.")
