cf5e7df656474f6f87adf47b3e07daf284311b40
Shital Tukaram Ambekar <shitalambekar013@gmail.com>
2026-07-20 23:23:19 +0530

    Create number_checker.py
    Add Day03 Number Checker program

---
+++ Day03/number_checker.py
@@ -0,0 +1,14 @@
+# ----------------------------------------
+# Day 03 - Number Checker
+# Author: Shital Tukaram Ambekar
+# Description: Check positive, negative or zero.
+# ----------------------------------------
+
+number = int(input("Enter a number: "))
+
+if number > 0:
+    print("Positive Number")
+elif number < 0:
+    print("Negative Number")
+else:
+    print("Zero")
