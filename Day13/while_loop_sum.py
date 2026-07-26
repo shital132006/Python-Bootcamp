a0b710bc3bbcd1d39ada340afd8846986ac7665e
Shital Tukaram Ambekar <shitalambekar013@gmail.com>
2026-07-26 21:43:52 +0530

    Create while_loop_sum.py
    Completed Day 13 - While Loop Sum

---
+++ Day13/while_loop_sum.py
@@ -0,0 +1,14 @@
+# ----------------------------------------
+# Day 13 - While Loop Sum
+# Author: Shital Tukaram Ambekar
+# Bootcamp: Python for Data Analyst
+# ----------------------------------------
+
+num = 1
+total = 0
+
+while num <= 5:
+    total = total + num
+    num += 1
+
+print("Sum =", total)
