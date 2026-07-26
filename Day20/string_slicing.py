62d76e9ecd164a5c60e2de3d71e051d824be235e
Shital Tukaram Ambekar <shitalambekar013@gmail.com>
2026-07-26 21:59:45 +0530

    Create string_slicing.py
    Completed Day 20 - String Slicing

---
+++ Day20/string_slicing.py
@@ -0,0 +1,14 @@
+# ----------------------------------------
+# Day 20 - String Slicing
+# Author: Shital Tukaram Ambekar
+# Bootcamp: Python for Data Analyst
+# ----------------------------------------
+
+word = "DataAnalysis"
+
+print("Original String :", word)
+
+print("First Part  :", word[0:4])
+print("Second Part :", word[4:12])
+print("Middle Part :", word[4:8])
+print("Last Part   :", word[8:12])
