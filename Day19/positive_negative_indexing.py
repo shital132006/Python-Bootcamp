7e34db3bdcdb026581ceafc8d2af373eebe12df9
Shital Tukaram Ambekar <shitalambekar013@gmail.com>
2026-07-26 21:57:52 +0530

    Create positive_negative_indexing.py
    Completed Day 19 - Positive and Negative Indexing

---
+++ Day19/positive_negative_indexing.py
@@ -0,0 +1,14 @@
+# ----------------------------------------
+# Day 19 - Positive & Negative Indexing
+# Author: Shital Tukaram Ambekar
+# Bootcamp: Python for Data Analyst
+# ----------------------------------------
+
+name = "Shital"
+
+print("Original String :", name)
+
+print("First Letter :", name[0])
+print("Third Letter :", name[2])
+print("Last Letter :", name[-1])
+print("Second Last Letter :", name[-2])
