7450f18143983a91a0c543ae4f218b85b57ecc7a
Shital Tukaram Ambekar <shitalambekar013@gmail.com>
2026-07-26 21:42:52 +0530

    Create while_loop_intro.py
    Completed Day 12 - While Loop Introduction

---
+++ Day12/while_loop_intro.py
@@ -0,0 +1,13 @@
+# ----------------------------------------
+# Day 12 - While Loop Introduction
+# Author: Shital Tukaram Ambekar
+# Bootcamp: Python for Data Analyst
+# ----------------------------------------
+
+count = 1
+
+while count <= 5:
+    print("Count =", count)
+    count += 1
+
+print("Loop Finished!")
