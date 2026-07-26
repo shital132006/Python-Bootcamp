d26e8b95c0c0dbaca3671a43eb5a69825017fc1d
Shital Tukaram Ambekar <shitalambekar013@gmail.com>
2026-07-26 21:55:07 +0530

    Create string_indexing.py
    Completed Day 18 - String Indexing Basics

---
+++ Day18/string_indexing.py
@@ -0,0 +1,13 @@
+# ----------------------------------------
+# Day 18 - String Indexing
+# Author: Shital Tukaram Ambekar
+# Bootcamp: Python for Data Analyst
+# ----------------------------------------
+
+name = "Shital Tukaram Ambekar"
+
+print(name)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
