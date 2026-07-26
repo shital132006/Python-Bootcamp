ffec270c11c2543724acebee634debaf7e23c0dc
Shital Tukaram Ambekar <shitalambekar013@gmail.com>
2026-07-26 21:24:55 +0530

    Create comparison_operators.py
    Completed Day 06 - Comparison Operators

---
+++ Day06/comparison_operators.py
@@ -0,0 +1,18 @@
+# ----------------------------------------
+# Day 06 - Comparison Operators
+# Author: Shital Tukaram Ambekar
+# Bootcamp: Python for Data Analyst
+# ----------------------------------------
+
+a = 10
+b = 20
+
+print("a =", a)
+print("b =", b)
+
+print("a == b :", a == b)
+print("a != b :", a != b)
+print("a > b  :", a > b)
+print("a < b  :", a < b)
+print("a >= b :", a >= b)
+print("a <= b :", a <= b)
